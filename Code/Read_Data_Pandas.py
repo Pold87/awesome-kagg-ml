f6d2ea046fc66d8afcd9abfcbcb350fd397da32a
Volker Strobel <volker.strobel87@gmail.com>
2015-02-12 21:12:26 +0100

    Changed path for data

--- Code/Read_Data_Pandas.py
+++ Code/Read_Data_Pandas.py
@@ -6,17 +6,16 @@ Created on Thu Feb 12 18:43:38 2015
 """
 
 import pandas as pd
+import os
 
 from matplotlib import pyplot as plt
-
-
 
 
 data_path = r"C:\Users\Trost\Copy\Nijmegen\Normal\Master\MasterAI\Pracitcal_ML\Data\drivers\1"
 
 rand = 1
 
-data = pd.read_csv(data_path + "\\" + str(rand) + ".csv")
+data = pd.read_csv(os.path.join(data_path, str(rand), ".csv"))
 
 print(data.head())
 
