955ec5b97aee586bac4802c492d731b99001aa2e
Thomas <wooohooo@gmx.de>
2015-02-12 19:20:33 +0100

    Added plotting

--- Code/Read_Data_Pandas.py
+++ Code/Read_Data_Pandas.py
@@ -7,8 +7,19 @@ Created on Thu Feb 12 18:43:38 2015
 
 import pandas as pd
 
-data_path = r"C:\Users\Trost\Copy\Nijmegen\Normal\Master\MasterAI\Pracitcal_ML\Data\drivers\1\1.csv"
+from matplotlib import pyplot as plt
 
-data = pd.read_csv(data_path)
 
-data.head()
+
+
+data_path = r"C:\Users\Trost\Copy\Nijmegen\Normal\Master\MasterAI\Pracitcal_ML\Data\drivers\1"
+
+rand = 1
+
+data = pd.read_csv(data_path + "\\" + str(rand) + ".csv")
+
+print(data.head())
+
+distance = data.x + data.y
+
+plt.plot(data.x,data.y)
