ed5e4f07c052aef4e9d59e1707aed5d38888360d
Volker Strobel <volker.strobel87@gmail.com>
2015-02-15 21:32:46 +0100

    Changed back to creating data frame by lists

--- Code/Load_Write.py
+++ Code/Load_Write.py
@@ -14,9 +14,9 @@ from os import listdir, path
 
 # All trips from driver 1 and 100 (to save time):
 # Linux:
-drivers_path = r"/home/pold/Documents/Radboud/kaggle/drivers_small/"
+drivers_path = r"/home/pold/Documents/Radboud/kaggle/drivers_small"
 # Windows:
-drivers_path = r"C:\Users\User\Documents\Radboud\kaggle\awesome-kagg-ml\drivers_small"
+# drivers_path = r"C:\Users\User\Documents\Radboud\kaggle\awesome-kagg-ml\drivers_small"
 
 drivers = listdir(drivers_path)
 
@@ -60,27 +60,37 @@ drivers = listdir(drivers_path)
 
 mega_df = pd.DataFrame()
 
+# Store all trips for all drivers in a pandas data frame (multiindeces for driver and index)
+# The lists are just for temporarily storing the data frames
+i = 0
+list_all_drivers_all_trips = []
+
 for driver in drivers:
 
+    i += 1
+    print(i)
+
+    list_one_driver_all_trips = []
     trips_path = path.join(drivers_path, driver)
     trips = listdir(trips_path)
 
     for trip in trips:
-
-        # Get file name without extension        
         trip_num = path.splitext(trip)[0]
         
-        # Read in trip
         df = pd.read_csv(path.join(trips_path, trip))
+        df_with_indices = pd.concat([df], keys = [(driver, trip_num)],
+                                          names = ('Driver', 'Trip'))
+
+        list_one_driver_all_trips.append(df_with_indices)
         
-        # Add trip to mega data frame
-        mega_df = pd.concat([mega_df, 
-                                    pd.concat([df],
-                                              keys = [(driver, trip_num)],
-                                              names = ('Driver', 'Trip'))])
-    
-# Save dataframe in HDF5 format
-mega_df.to_hdf('dataframe.h5','table')
+    df_one_driver = pd.concat(list_one_driver_all_trips, axis = 0, keys = trips)
+    list_all_drivers_all_trips.append(df_one_driver)
+
+df_all_drivers = pd.concat(list_all_drivers_all_trips, axis = 0, keys = drivers)
+
+# Pickle the dataframe
+df_all_drivers.to_hdf('dataframe.h5','table')
+
 
 
 
