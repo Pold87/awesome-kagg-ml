e08a2a9227d8168ee3073a5750dd1e3b2ccb27c9
Volker Strobel <volker.strobel87@gmail.com>
2015-03-03 17:38:06 +0100

    minor stuff

--- Code/draw_repeated_trip.py
+++ Code/draw_repeated_trip.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env pypy
+
 # Originaly developed by rcarson
 # aixueer4ever@gmail.com
 # based on http://www.kaggle.com/c/axa-driver-telematics-analysis/forums/t/12321/python-code-to-automatically-draw-repeated-trips 
@@ -9,8 +11,6 @@
 # added numpy and sklearn api where applicable
 # use of hypot and arctan2 functions
 
-from numba.decorators import jit
-from numba import double
 import os
 import numpy as np
 import pandas as pd
@@ -28,13 +28,13 @@ def rotational(theta):
     # http://en.wikipedia.org/wiki/Affine_transformation#mediaviewer/File:2D_affine_transformation_matrix.svg
     return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-
 class draw_repeated_trip:
 
-    def __init__(self,dataPath,driver=None, df_submission = None, threshold=0.05):
+    def __init__(self,dataPath,driver=None, threshold=0.05, df_submission = None):
         self.dataPath=dataPath
         self.driver=driver
         self.threshold=threshold
+        self.df_submission = df_submission
 
     def flip(self,x):
         # flip a trip if more that half of coordinates have y axis value above 0 
@@ -60,7 +60,7 @@ class draw_repeated_trip:
         [x,y]=a
         return np.hypot(x,y), np.arctan2(y,x)
 
-    def update_trips(self,tripl,trip,tripname,tripcounter,tripother):
+    def update_trips(self, tripl,trip, tripname, tripcounter, tripother):
         if len(tripl)==0:
             tripl[tripname]=trip
             tripcounter[tripname]=0
@@ -68,65 +68,65 @@ class draw_repeated_trip:
         else:
             for t in tripl:
                 if self.sametrip(tripl[t],trip):
-                    tripcounter[t]+=1
+                    tripcounter[t] += 1
                     tripother[t].append(tripname)
                     for xx in tripother[t]:
-                        tripcounter[xx]=tripcounter[t]
-                    return tripl,tripcounter,tripother
-            tripl[tripname]=trip
-            tripcounter[tripname]=0
-            tripother[tripname]=[]
-        return tripl,tripcounter,tripother            
+                        tripcounter[xx] = tripcounter[t]
+                    return tripl, tripcounter, tripother
+            tripl[tripname] = trip
+            tripcounter[tripname] = 0
+            tripother[tripname] = []
+        return tripl, tripcounter, tripother
 
 
     def getdd(self,tx,ty):
-	# calculate distances and pad shortest array
-        # pad shortest array with it's last value
+        """
+        calculate distances and pad shortest array
+        pad shortest array with it's last value
+        """
         gap = tx.shape[0]-ty.shape[0]
-        if gap>0:
-            ty=np.pad(ty, ((0,gap),(0,0)), 'edge')
+        if gap > 0:
+            ty = np.pad(ty, ((0, gap), (0, 0)), 'edge')
         elif gap <0:
-            tx=np.pad(tx, ((0,-gap),(0,0)), 'edge')
+            tx = np.pad(tx, ((0, -gap), (0, 0)), 'edge')
         else:
             pass
         # use any distance metric that you would like
-        return paired_distances(tx,ty,metric='l2').sum()
+        return paired_distances(tx, ty, metric='l2').sum()
 
-    def sametrip(self,tx,ty):
-        mm=int(tx.shape[0]*self.threshold)
-        txx=np.vstack((tx[mm:,:],tx[-mm:,:]))
-        dd=self.getdd(tx,txx)
-        return  self.getdd(tx,ty)<=dd
-    def check_trips(self,tripcounter,tripname):
+    def sametrip(self, tx, ty):
+        mm = int(tx.shape[0]*self.threshold)
+        txx = np.vstack((tx[mm:, :], tx[-mm:, :]))
+        dd = self.getdd(tx, txx)
+        return self.getdd(tx, ty) <= dd
+
+    def check_trips(self, tripcounter, tripname):
         return tripcounter[tripname] > 2
 
     def draw(self):
-        path=self.dataPath
-        drivers=os.listdir(path)
+        path = self.dataPath
+        drivers = os.listdir(path)
         if self.driver!='-1':
-           driver=self.driver
+           driver = self.driver
         else:
-           driver=drivers[randrange(len(drivers))]
-        print ('driver',driver)
+           driver = drivers[randrange(len(drivers))]
+        print('driver',driver)
         trips=os.listdir(path+'/'+driver)
-        tripl={} # trip name -> trip data
-        tripc={} # trip name -> number of same trips
-        tripo={} # trip name -> list of names of other same trips
+        tripl = {} # trip name -> trip data
+        tripc = {} # trip name -> number of same trips
+        tripo = {} # trip name -> list of names of other same trips
         for trip in trips:
             if '.csv' in trip:
-                t1=pd.read_csv(path+'/'+driver+'/'+trip)
-                t1r=self.flip(self.rotate_trip(t1))
-                tripl,tripc,tripo=self.update_trips(tripl,t1r,trip,tripc,tripo)
+                t1 = pd.read_csv(path+'/'+driver+'/'+trip)
+                t1r = self.flip(self.rotate_trip(t1))
+                tripl, tripc, tripo = self.update_trips(tripl, t1r, trip, tripc, tripo)
         c=0
         for trip in trips:
             if '.csv' in trip:
-                t1=pd.read_csv(path+'/'+driver+'/'+trip)
-                plt.subplot(1,3,1)
-                plt.plot(t1['x'],t1['y'])
-                plt.title('all trips of driver '+driver)
-                t1r= self.flip(self.rotate_trip(t1))
-                if self.check_trips(tripc,trip):
-                    df_submission.loc[str(driver) + '_' + str(trip), 'prob'] = 1
+                t1 = pd.read_csv(path+'/'+driver+'/'+trip)
+                t1r = self.flip(self.rotate_trip(t1))
+                if self.check_trips(tripc, trip):
+                    self.df_submission.loc[str(driver) + '_' + str(trip), 'prob'] = 1
 
 
 if __name__ == "__main__":
@@ -135,12 +135,11 @@ if __name__ == "__main__":
     drivers_path = path.join("..", "drivers")
     drivers = listdir(drivers_path)
 
-    df_submission = pd.read_csv('submission-6.csv')
-
-
-
     for driver in drivers:
-        dd= draw_repeated_trip(dataPath=mypath, driver=driver, df_submission = df_submission, threshold=float(0.05))
+        dd= draw_repeated_trip(dataPath=mypath,
+                               driver=driver,
+                               threshold=float(0.05),
+                               df_submission = pd.read_csv('submission-6.csv'))
         dd.draw()
 
-    df_submission.to_csv('submission_processed_6.csv', index = False)
+    dd.df_submission.to_csv('submission_processed_6.csv', index = False)
