f0d44280b2c90251ee69257b152ec4e313e2dde1
Arjen Zijlstra <zijlstra.arjen@gmail.com>
2015-04-09 01:33:18 +0200

    parse csv, logloss, build testdata

---
+++ Otto_Matlab/logloss.m
@@ -0,0 +1,18 @@
+function [ ll ] = logloss( p, y )
+%LOGLOSS This is the multi-class version of the Logarithmic Loss metric. 
+%  Each observation is in one class and for each observation, you submit a 
+%  predicted probability for each class. The metric is negative the log 
+%  likelihood of the model that says each test observation is chosen 
+%  independently from a distribution that places the submitted probability 
+%  mass on the corresponding class, for each observation.
+% 
+%  N is the number of observations, M is the number of class labels, log 
+%  is the natural logarithm, y(i, j) is 1 if observation i is in class j 
+%  and 0 otherwise, and p(i, j) is the predicted probability that 
+%  observation i is in class j.
+
+   [N, M] = size(p);
+   ll = -N  \ sum(sum(y .* log(p)));
+
+end
+
--- Otto_Matlab/script.m
+++ Otto_Matlab/script.m
@@ -1,17 +1,19 @@
 % data = csvread('train.csv', 100, 100, [1 1 3 3]);
-%% 
 
-filename = 'test.csv'
+%% read/parse the data
+
+filename = 'train.csv'
 fid = fopen(filename);
 data = fread(fid, '*char')';  % read all contents into data as a char array (don't forget the `'` to make it a row rather than a column).
 fclose(fid);
 entries = regexp(data, '\n', 'split');
 
 parsed = regexp(entries(1), ',', 'split');
-header = header{1};
-id_ = header(1);
+parsed = parsed{1};
+id_ = parsed(1);
 
 n = length(entries) - 1; % last line is wrongly parsed
+% n = 100;
 if filename == 'train.csv'
    size = n - 1;
 else
@@ -25,9 +27,9 @@ else
    header = header(2:end);
 end
 
-id = zeros(n - 1, 1);
-features = zeros(n - 1, length(header));
-classes = cell(n - 1, 1);
+id = zeros(size, 1);
+features = zeros(size, length(header));
+classes = cell(size, 1);
 
 for i = 2:n
    if mod(i, 100) == 0
@@ -36,13 +38,76 @@ for i = 2:n
    parsed = regexp(entries(i), ',', 'split');
    parsed = parsed{1};
    id(i - 1) = cellfun(@str2num, parsed(1));
-   features(i - 1, :) = cellfun(@str2num, parsed(2:size));
    if size == n - 1
+      features(i - 1, :) = cellfun(@str2num, parsed(2:end - 1));
       classes(i - 1) = parsed(end);
+   else
+      features(i - 1, :) = cellfun(@str2num, parsed(2:end));
    end
 end
 
 
+%% classify
 
+train = features(1:2:end, :);
+trainlabelscell = classes(1:2:end, :);
+test = features(1:2:end, :);
+testlabelscell = classes(1:2:end, :);
+
+
+% -- bah
+trainlabels = zeros(length(trainlabelscell), 1);
+for i = 1:length(trainlabelscell)
+   trainlabels(i) = str2num(trainlabelscell{i}(end:end));
+end
+
+testlabels = zeros(length(testlabelscell), 1);
+for i = 1:length(testlabelscell)
+   testlabels(i) = str2num(testlabelscell{i}(end:end));
+end
+
+y = zeros(length(testlabelscell), 9);
+for i = 1:length(testlabels)
+   y(i, testlabels(i)) = 1;
+end
+% -- tot hier bah
+
+net = newff(train, trainlabels, 5);
+net = train(net, train, trainlabels);
+outputs = net(inputs);
+
+%% 
+y = [
+   [1, 0, 0, 0, 0, 0, 0, 0, 0];
+   [1, 0, 0, 0, 0, 0, 0, 0, 0];
+   [0, 1, 0, 0, 0, 0, 0, 0, 0];
+   [1, 0, 0, 0, 0, 0, 0, 0, 0];
+   [0, 0, 0, 0, 1, 0, 0, 0, 0];
+   [0, 1, 0, 0, 0, 0, 0, 0, 0];
+   [0, 0, 1, 0, 0, 0, 0, 0, 0];
+   [0, 0, 0, 1, 0, 0, 0, 0, 0];
+   [0, 0, 0, 1, 0, 0, 0, 0, 0];
+   [0, 0, 0, 0, 1, 0, 0, 0, 0];
+   [0, 0, 0, 0, 1, 0, 0, 0, 0];
+   [0, 0, 0, 0, 0, 1, 0, 0, 0];
+   [0, 0, 0, 0, 0, 1, 0, 0, 0];
+   [0, 0, 0, 0, 0, 1, 0, 0, 0];
+   [0, 0, 0, 0, 0, 0, 1, 0, 0];
+   [0, 0, 0, 0, 0, 0, 0, 1, 0];
+   [0, 0, 0, 0, 0, 0, 0, 0, 1];
+   [0, 0, 0, 0, 0, 0, 0, 0, 1];
+   [0, 0, 0, 0, 0, 0, 0, 0, 1];
+   [0, 0, 0, 0, 0, 0, 0, 1, 0];
+   [0, 0, 0, 0, 0, 0, 1, 0, 0];
+   [0, 0, 1, 0, 0, 0, 0, 0, 0];
+   [0, 1, 0, 0, 0, 0, 0, 0, 0]];
+
+[n m] = size(y);
+
+p = magic(max(n, m));
+p = p(1:n, 1:m);
+p = p / max(max(p));
+
+logloss(p, y)
 
 
