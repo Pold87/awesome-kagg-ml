59549e1e8f46daf07c966a109934828f81f86418
Edward Betts <edward@4angle.com>
2016-04-10 06:46:26 +0100

    use % formatter

--- matlab/done.py
+++ matlab/done.py
@@ -1,3 +1,3 @@
 import subprocess
 
-print("{:.2f}% done".format(int(subprocess.check_output(['wc', '-l', 'test1.csv']).decode("utf-8").split(" ")[0]) / (200 * 2738) * 100))
+print("{:.2%} done".format(int(subprocess.check_output(['wc', '-l', 'test1.csv']).decode("utf-8").split(" ")[0]) / (200 * 2738)))
